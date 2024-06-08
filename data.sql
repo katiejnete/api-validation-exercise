7be4f91c78e90d0402479df74ac5126c43fc4d6c
Kate Vong <katiejnete@gmail.com>
2024-06-07 22:40:08 -0700

    complete schemas

---
+++ data.sql
@@ -0,0 +1,12 @@
+\c books
+
+CREATE TABLE books (
+  isbn TEXT PRIMARY KEY,
+  amazon_url TEXT,
+  author TEXT NOT NULL,
+  language TEXT NOT NULL, 
+  pages INTEGER NOT NULL,
+  publisher TEXT NOT NULL,
+  title TEXT NOT NULL, 
+  year INTEGER NOT NULL
+);
